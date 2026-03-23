da765ba0b6414a7e4655763d1192e0f9e85b563f
kdowd <kevin.dowd@gmail.com>
2026-03-24 11:36:37 +1300

    added string ouput agin

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 {
     string message = "Hello, World 2026 !!";
     cout << string(100, '#');
+    cout << string(1000, '#');
 
     cout << message << "\t" << message.length() << endl;
     return 0;
